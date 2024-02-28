d6f749d2d44e04ea469b795fc4164ede91a5d2d8
Albert Chubakov <a.chubakov@enablon.com>
2024-02-28 13:47:22 +0100

    Initial Commit

---
+++ WeatherInParis/WeatherInParisApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherInParisApp.swift
+//  WeatherInParis
+//
+//  Created by Chubakov, Albert on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherInParisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
